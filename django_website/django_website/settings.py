b064183b5b8f8b13583fc1afe27a1f96b13c7d2b
Patrick <pkbruso@gmail.com>
2021-10-13 17:50:36 -0700

    Add login redirect url

--- django_website/django_website/settings.py
+++ django_website/django_website/settings.py
@@ -128,3 +128,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'  # change from boostrap4 to bootstrap5? but need to double check
+
+LOGIN_REDIRECT_URL = 'blog-home'
